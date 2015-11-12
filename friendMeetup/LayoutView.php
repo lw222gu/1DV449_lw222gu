ec943e4cedfdc277d85e17a41a41171c02c5fb88
Lisa Westlund <lw222gu@student.lnu.se>
2015-11-12 16:37:00 +0100

    Gets links from application startpage

--- friendMeetup/LayoutView.php
+++ friendMeetup/LayoutView.php
@@ -36,7 +36,7 @@ class LayoutView {
         if(isset($_POST["url"])){
             $url = $_POST["url"];
             $result = new ScrapeResult($url);
-            return $result->curlRequest();
+            return $result->getResult();
         }
         return "";
     }
--- friendMeetup/ScrapeResult.php
+++ friendMeetup/ScrapeResult.php
@@ -3,17 +3,57 @@
 class ScrapeResult{
 
     private $url;
+    private $data;
+    private $result = "";
+    private $calendarLink;
+    private $cinemaLink;
+    private $restaurantLink;
 
     public function __construct($url){
         $this->url = $url;
+        $this->curlRequest();
+        $this->getLinks();
     }
 
-    public function curlRequest(){
+    private function curlRequest(){
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $this->url);
-        $data = curl_exec($ch);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        $this->data = curl_exec($ch);
         curl_close($ch);
-        return $data;
+    }
+
+    private function getLinks(){
+        $dom = new DOMDocument();
+
+        if($dom->loadHTML($this->data)){
+            $xPath = new DOMXPath($dom);
+            $links = $xPath->query('//ol//li/a');
+
+            foreach ($links as $link){
+                if($link->nodeValue == "Kalendrar"){
+                    $this->calendarLink = $link->getAttribute("href");
+                    continue;
+                }
+
+                if($link->nodeValue == "Stadens biograf!"){
+                    $this->cinemaLink = $link->getAttribute("href");
+                    continue;
+                }
+
+                if($link->nodeValue == "Zekes restaurang!"){
+                    $this->restaurantLink = $link->getAttribute("href");
+                    continue;
+                }
+            }
+        }
+        else {
+            die("Fel vid inläsning av data.");
+        }
+    }
+
+    public function getResult(){
+        return $this->result;
     }
 
 }
