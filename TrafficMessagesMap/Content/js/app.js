3c8c297d4b939783d5e160f4ffb4902dba456251
Lisa Westlund <lw222gu@student.lnu.se>
2015-12-12 19:00:56 +0100

    Centers map according to user location

--- TrafficMessagesMap/Content/js/app.js
+++ TrafficMessagesMap/Content/js/app.js
@@ -3,50 +3,56 @@
 var TrafficMap = {
 
   json: {},
-  map: L.map('map', {
-      center: [63.0, 14.0], //latitude and longitude for Östersund, the middle of Sweden
-      minZoom: 2,
-      zoom: 5
-  }),
+  map: {},
+  lat: 59.0,
+  long: 15.0,
   icons: {},
-  selection: "4",
+  selection: document.getElementById("select-category").value,
   markers: [],
   markersGroup: L.layerGroup(this.markers),
   //options: ["Vägtrafik": "0", "Kollektivtrafik": "1", "Planerad störning":"2", "Övrigt": "3", "Visa alla kategorier": null],
 
   init: function(){
+    TrafficMap.map =  L.map('map', {
+      center: [TrafficMap.lat, TrafficMap.long], //standard latitude and longitude
+      minZoom: 2,
+      zoom: 5
+    });
 
-    L.tileLayer( 'http://{s}.mqcdn.com/tiles/1.0.0/map/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="http://osm.org/copyright" title="OpenStreetMap" target="_blank">OpenStreetMap</a> contributors | Tiles Courtesy of <a href="http://www.mapquest.com/" title="MapQuest" target="_blank">MapQuest</a> <img src="http://developer.mapquest.com/content/osm/mq_logo.png" width="16" height="16">',
-        subdomains: ['otile1','otile2','otile3','otile4']
-    }).addTo( TrafficMap.map );
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(TrafficMap.getPosition);
+    }
+
+    L.tileLayer('http://{s}.mqcdn.com/tiles/1.0.0/map/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="http://osm.org/copyright" title="OpenStreetMap" target="_blank">OpenStreetMap</a> contributors | Tiles Courtesy of <a href="http://www.mapquest.com/" title="MapQuest" target="_blank">MapQuest</a> <img src="http://developer.mapquest.com/content/osm/mq_logo.png" width="16" height="16">',
+      subdomains: ['otile1','otile2','otile3','otile4']
+    }).addTo(TrafficMap.map);
 
     var select = document.getElementById("select-category");
     select.onchange = function(){
-      TrafficMap.selection = select[select.selectedIndex].value;
-      TrafficMap.map.removeLayer("markersGroup");
-      TrafficMap.renderPointers();
+      TrafficMap.selection = this.value;
+      TrafficMap.renderMarkers();
     };
 
-    TrafficMap.createPointerIcons();
-    TrafficMap.renderPointers();
+    TrafficMap.createMarkerIcons();
+    TrafficMap.renderMarkers();
   },
 
   getJson: function(){
     var xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function(){
-        if (xhr.readyState === 4 && xhr.status === 200){
-            TrafficMap.json = JSON.parse(xhr.responseText);
-        }
+      if (xhr.readyState === 4 && xhr.status === 200){
+        TrafficMap.json = JSON.parse(xhr.responseText);
+      }
     };
     xhr.open("GET", "Resources/trafficMessages.json", false);
     xhr.send(null);
   },
 
-  renderPointers: function(){
+  renderMarkers: function(){
 
-    TrafficMap.markers.forEach(function (mark) {
-        TrafficMap.map.removeLayer(mark);
+    TrafficMap.markers.forEach(function(mark){
+      TrafficMap.map.removeLayer(mark);
     });
 
     TrafficMap.getJson();
@@ -69,13 +75,13 @@ var TrafficMap = {
     }
   },
 
-  createPointerIcons: function(){
+  createMarkerIcons: function(){
 
     var Icon = L.Icon.extend({
       options: {
-          iconSize:     [25, 24],
-          iconAnchor:   [8, 23],
-          popupAnchor:  [-3, -76]
+        iconSize:     [25, 24],
+        iconAnchor:   [8, 23],
+        popupAnchor:  [-3, -76]
       }
     });
 
@@ -90,6 +96,12 @@ var TrafficMap = {
       "2": planeradStorningIcon,
       "3": ovrigtIcon
     };
+  },
+
+  getPosition: function(position){
+    TrafficMap.lat = position.coords.latitude;
+    TrafficMap.long = position.coords.longitude;
+    TrafficMap.map.setView([TrafficMap.lat, TrafficMap.long], 7);
   }
 
 };
